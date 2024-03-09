0bb9bd0efddcff0969fbf8893201ad11415e8b3d
Pushhpendra Kumar <113991886+pushpendrakumar1@users.noreply.github.com>
2024-03-10 00:43:54 +0530

    Add files via upload

---
+++ schema/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for schema project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schema.settings')
+
+application = get_wsgi_application()
